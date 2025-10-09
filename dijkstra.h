d94cbd736a6d79f89614165505a4f58f23af5712
SnehaaGarg <snehagarg1609@gmail.com>
2025-10-09 17:16:26 +0530

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -2,6 +2,7 @@
 #define DIJKSTRA_H
 #include "graph.h"
 #include "priority_queue.h"
-int compute_shortest_path(int start, int end, int* distances, int* previous, int* total_time);  // Returns 1 if path found, 0 else
-int reconstruct_path(int end, int* previous, char** path, int* path_len);  // Builds path array (reversed)
+int compute_shortest_path(int start, int end, int* distances, int* previous, int* total_time); 
+int reconstruct_path(int end, int* previous, char** path, int* path_len); 
+
 #endif
