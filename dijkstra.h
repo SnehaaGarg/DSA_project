97de944a9b53ee7a39984b3b14fc2b533d4b3b03
SnehaaGarg <snehagarg1609@gmail.com>
2025-10-28 00:27:55 +0530

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -1,8 +1,17 @@
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
+
 #include "graph.h"
-#include "priority_queue.h"
-int compute_shortest_path(int start, int end, int* distances, int* previous, int* total_time); 
-int reconstruct_path(int end, int* previous, char** path, int* path_len); 
+
+#define INF 1e9
+
+typedef struct {
+    double distance;
+    int previous_node;
+    int visited;
+} PathInfo;
+
+void dijkstra_shortest_path(Graph *graph, int start_node, int end_node, 
+                           PathInfo path_info[], int path_nodes[], int *path_length);
 
 #endif
