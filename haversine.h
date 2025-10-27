9e1466f9cffef99338b78240a04366a1ce674618
SnehaaGarg <snehagarg1609@gmail.com>
2025-10-28 00:35:25 +0530

    Update haversine.h

--- haversine.h
+++ haversine.h
@@ -1,9 +1,10 @@
 #ifndef HAVERSINE_H
 #define HAVERSINE_H
-
+ 
 #define EARTH_RADIUS_KM 6371.0
 
 double degrees_to_radians(double degrees);
 double haversine_distance(double lat1, double lon1, double lat2, double lon2);
 
 #endif
+
